82c1f6f86cfa7da527df262380911138deae379b
Zero-One101 <stephenbadrick@gmail.com>
2016-01-04 17:30:43 +0000

    Initial project setup

---
+++ Source/StealthyAssessment/StealthyAssessment.h
@@ -0,0 +1,8 @@
+// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+
+#ifndef __STEALTHYASSESSMENT_H__
+#define __STEALTHYASSESSMENT_H__
+
+#include "EngineMinimal.h"
+
+#endif
---
+++ Source/StealthyAssessment/StealthyAssessmentGameMode.h
@@ -0,0 +1,16 @@
+// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+#pragma once
+#include "GameFramework/GameMode.h"
+#include "StealthyAssessmentGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AStealthyAssessmentGameMode : public AGameMode
+{
+	GENERATED_BODY()
+
+public:
+	AStealthyAssessmentGameMode();
+};
+
+
+
