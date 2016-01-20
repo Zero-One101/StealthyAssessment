c82fbade6c4dc4c53cde650c188a0ac3fdbb0920
Stephen Badrick <stephenbadrick@gmail.com>
2016-01-20 10:36:10 +0000

    Minor change to eventually test ragdolling
    Encountered a bug with VS/VAX where I couldn't type any more. 10/10

--- Source/StealthyAssessment/EnemyPatrolAIController.cpp
+++ Source/StealthyAssessment/EnemyPatrolAIController.cpp
@@ -1,6 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "StealthyAssessment.h"
+#include "EnemyPatrol.h"
+#include "Runtime/Engine/Classes/Components/StaticMeshComponent.h"
 #include "EnemyPatrolAIController.h"
 
 AEnemyPatrolAIController::AEnemyPatrolAIController(const class FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
@@ -21,15 +23,16 @@ void AEnemyPatrolAIController::BeginPlay()
 
 void AEnemyPatrolAIController::Tick(float DeltaSeconds)
 {
-	if (Player)
-	{
-		SetFocus(Player);
-		MoveToActor(Player);
-	}
+	//if (Player)
+	//{
+	//	SetFocus(Player);
+	//	MoveToActor(Player);
+	//}
 	UpdateControlRotation(DeltaSeconds);
 }
 
 void AEnemyPatrolAIController::OnMoveCompleted(FAIRequestID RequestID, EPathFollowingResult::Type Result)
 {
-
+	class AEnemyPatrol* owner = (AEnemyPatrol*)GetOwner();
+	TArray<UStaticMeshComponents*> Components;
 }
